cb28c5a37c336af25264ea6a5047a9a4cec430f6
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2021-12-20 16:17:31 +0900

    Update ethereum_api.sql

--- ethereum_api.sql
+++ ethereum_api.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100417
  File Encoding         : 65001
 
- Date: 20/12/2021 15:15:12
+ Date: 20/12/2021 16:17:26
 */
 
 SET NAMES utf8mb4;
@@ -22,13 +22,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `eth_pairs`;
 CREATE TABLE `eth_pairs`  (
-  `token0Address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '0',
+  `token0Address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `token1Address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `factoryAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `lastPrice` float NULL DEFAULT 0,
   `timestamp` int(20) NULL DEFAULT 0,
-  `fee` int(255) NULL DEFAULT 0,
   PRIMARY KEY (`pairAddress`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -37,8 +36,9 @@ CREATE TABLE `eth_pairs`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `eth_tokens`;
 CREATE TABLE `eth_tokens`  (
-  `tokenAddres` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tokenAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `tokenSymbol` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tokenName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `tokenDecimals` int(255) NULL DEFAULT NULL,
   `totalSupply` bigint(255) NULL DEFAULT NULL,
   `owner` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL

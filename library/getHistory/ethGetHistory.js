b19b368f8ab41d9f8740065b8704ac2192abe225
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-02-06 11:24:46 +0900

    Update ethGetHistory.js

--- library/getHistory/ethGetHistory.js
+++ library/getHistory/ethGetHistory.js
@@ -1239,11 +1239,11 @@ async function getOneTokenScanInfos(tokenAddress, proxy) {
 async function getTokenScanInfos() {
     var tokens = await knex('eth_tokens').orderBy('createdAt', 'desc').select('*')
 
-    for (var i = 0; i < tokens.length; i += 5) {
+    for (var i = 0; i < tokens.length; i += 28) {
         myLogger.log(i)
         var funcs = []
 
-        for (var j = i; j < i + 5 && j < tokens.length; j ++) {
+        for (var j = i; j < i + 28 && j < tokens.length; j ++) {
             funcs.push(getOneTokenScanInfos(tokens[j].tokenAddress, config.PROXY[j - i]))
             // await getOneTokenScanInfos(tokens[j].tokenAddress, config.PROXY[j - i])
         }

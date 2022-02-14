55a93a37dbdc1a60978c20943706cf0def38a4b9
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-02-14 19:20:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

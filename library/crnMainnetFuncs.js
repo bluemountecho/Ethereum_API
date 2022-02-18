7861057f20932aba38ea8178fcc0eceec09f402a
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-02-18 20:11:46 +0900

    Update crnMainnetFuncs.js

--- library/crnMainnetFuncs.js
+++ library/crnMainnetFuncs.js
@@ -728,7 +728,8 @@ module.exports.getLiveTokenPrice = async function getLiveTokenPrice(tokenAddr) {
 module.exports.getLivePairPrice = async function getLivePairPrice(pairAddr, time) {
     try {
         var pair = pairAddr.toLowerCase()
-        var rows = await knex('eth_live').where('pairAddress', pair).where('swapAt', '>', time).orderBy('swapAt', 'asc').select('*')
+        var cur = new Date(time).toISOString().replace(/T/, ' ').replace(/\..+/, '')
+        var rows = await knex('eth_live').where('pairAddress', pair).where('swapAt', '>', cur).orderBy('swapAt', 'asc').select('*')
         var datas = []
         // getPriceOfToken
 

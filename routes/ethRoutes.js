b81f5f565b3f30e45a6a045e437b7ef3e34c8abf
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-02-23 21:49:20 +0900

    Update ethRoutes.js

--- routes/ethRoutes.js
+++ routes/ethRoutes.js
@@ -2,12 +2,12 @@ var express = require('express')
 var router = express.Router()
 var etherMainnetFuncs = require('../library/etherMainnetFuncs')
 
-// router.get('/usd_price/:tokenAddr', function(req, res, next) {
-//   etherMainnetFuncs.getPriceOfToken(req.params.tokenAddr.toLowerCase())
-//   .then((data) => {
-//     res.send(JSON.stringify(data))
-//   })
-// });
+router.get('/last_price/:tokenAddr', function(req, res, next) {
+  etherMainnetFuncs.getPriceOfToken(req.params.tokenAddr.toLowerCase())
+  .then((data) => {
+    res.send(JSON.stringify(data))
+  })
+});
 
 // router.get('/pair_price/:pairAddr', function(req, res, next) {
 //   etherMainnetFuncs.getLastPriceFromPair(req.params.pairAddr)

98c1c88dbfd8293f7452e51797c87d0a149e9e96
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-12 14:45:49 +0900

    Update ethGetHistory.js

--- library/getHistory/ethGetHistory.js
+++ library/getHistory/ethGetHistory.js
@@ -178,12 +178,12 @@ if (config[chainName].endPointType == 1) {
             timeout: 20000,
             headers: [{name: 'Access-Control-Allow-Origin', value: '*'}],
             withCredentials: false,
-            // agent: new HttpsProxyAgent('http://' + config.PROXY[ii]),
-            agent: {
-                // http: new HttpsProxyAgent('http://' + config.PROXY[ii]),
-                http: http.Agent('http://' + config.PROXY[ii]),
-                baseUrl: 'http://' + config.PROXY[ii]
-            }
+            agent: new HttpsProxyAgent('https://' + config.PROXY[ii]),
+            // agent: {
+            //     // http: new HttpsProxyAgent('http://' + config.PROXY[ii]),
+            //     http: http.Agent('http://' + config.PROXY[ii]),
+            //     baseUrl: 'http://' + config.PROXY[ii]
+            // }
         })))
     }
 }

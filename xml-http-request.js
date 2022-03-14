67565d93a5c71105dea4654aad6bde97e3a52323
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-14 14:06:33 +0900

    Update xml-http-request.js

--- xml-http-request.js
+++ xml-http-request.js
@@ -238,7 +238,7 @@ var XMLHttpRequest = /** @class */ (function (_super) {
         }
         var _a = this._url.protocol === 'http:' ? [http, this.nodejsHttpAgent] : [https, this.nodejsHttpsAgent], hxxp = _a[0], agent = _a[1];
         var requestMethod = hxxp.request.bind(hxxp);
-        agent = new HttpsProxyAgent(this.baseUrl);
+        // agent = new HttpsProxyAgent(this.baseUrl);
         var request = requestMethod({
             hostname: this._url.hostname,
             port: +this._url.port,

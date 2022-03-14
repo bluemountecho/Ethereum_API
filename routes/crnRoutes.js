bcdd0b2eed7b0b4a4a890650bf7a7549c428757b
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-14 16:10:46 +0900

    Update crnRoutes.js

--- routes/crnRoutes.js
+++ routes/crnRoutes.js
@@ -11,7 +11,7 @@ router.get('/token_info/:tokenAddr', function(req, res, next) {
 
 router.get('/token_price', function(req, res, next) {
   console.log(req.query)
-  etherMainnetFuncs.getLiveTokenPrice(req.query.symbol, req.query.symbol, req.query.interval, req.query.startTime, req.query.endTime, req.query.limit)
+  etherMainnetFuncs.getChartPriceData(req.query.symbol, req.query.symbol, req.query.interval, req.query.startTime, req.query.endTime, req.query.limit)
   .then((data) => {
     res.send(JSON.stringify(data))
   })

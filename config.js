7dd73d5e2f2e60af449704749548bcf78ba3aece
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-19 01:38:07 +0900

    test

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports.bsc = {
         'https://bsc-dataseed.binance.org/',
     ],
     'endPointType': 1,
-    'FROMBLOCK': 0,
+    'FROMBLOCK': 9247154,
     'TOBLOCK': 16104144,
     'TRANSACTIONV1': 13600,
     'TRANSACTIONV2': 200,

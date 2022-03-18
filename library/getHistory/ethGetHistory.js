c39728818871def5855f73e92e6f6056c2622d50
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-18 10:07:26 +0900

    Update ethGetHistory.js

--- library/getHistory/ethGetHistory.js
+++ library/getHistory/ethGetHistory.js
@@ -1458,12 +1458,12 @@ async function getContavoInfo() {
 
 async function init() {
     await getAllPairs(FROMBLOCK)
-    await getTokenAndPairData()
+    // await getTokenAndPairData()
     
-    myLogger.log('Getting token and pair data finished!')
-    myLogger.log(FROMBLOCK + '~' + TOBLOCK + ' ' + pastTableName)
+    // myLogger.log('Getting token and pair data finished!')
+    // myLogger.log(FROMBLOCK + '~' + TOBLOCK + ' ' + pastTableName)
 
-    await getTransactionHistory(FROMBLOCK)
+    // await getTransactionHistory(FROMBLOCK)
 }
 
 init()

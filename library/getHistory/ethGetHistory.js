46e905940f227326e26dcdec2c4b6e6cc9df6855
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-28 15:20:17 +0900

    Update ethGetHistory.js

--- library/getHistory/ethGetHistory.js
+++ library/getHistory/ethGetHistory.js
@@ -1514,9 +1514,11 @@ async function getUSDPrice() {
                 or (token1Address = "' + rows[i].token0Address + '" and token0Address="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")\
             ')).select('*')
 
-            vis[rows[i].token0Address] = true
+            vis[rows[i].token0Address] = 1
 
             if (rows1.length > 0) continue
+
+            vis[rows[i].token0Address] = 2
             baseTokens.push(rows[i].token0Address)
         }
         
@@ -1526,17 +1528,18 @@ async function getUSDPrice() {
                 or (token1Address = "' + rows[i].token1Address + '" and token0Address="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")\
             ')).select('*')
 
-            vis[rows[i].token1Address] = true
+            vis[rows[i].token1Address] = 1
 
             if (rows1.length > 0) continue
-            
+
+            vis[rows[i].token0Address] = 2
             baseTokens.push(rows[i].token1Address)
         }
     }
 
     console.log('Finished')
 
-    myLogger.log(baseTokens)
+    myLogger.log(vis)
 }
 
 async function init() {

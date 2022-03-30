fad54828c00503b7150f32b18797de10255a9042
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-03-30 14:39:20 +0900

    test

--- library/getHistory/ethGetHistory.js
+++ library/getHistory/ethGetHistory.js
@@ -1704,14 +1704,14 @@ async function getUSDPrice() {
 async function init() {
     // await getAllPairs(FROMBLOCK)
 
-    // await getTokenAndPairData()
+    await getTokenAndPairData()
     
-    // myLogger.log('Getting token and pair data finished!')
-    // myLogger.log(FROMBLOCK + '~' + TOBLOCK + ' ' + pastTableName)
+    myLogger.log('Getting token and pair data finished!')
+    myLogger.log(FROMBLOCK + '~' + TOBLOCK + ' ' + pastTableName)
 
-    // await getTransactionHistory(FROMBLOCK)
+    await getTransactionHistory(FROMBLOCK)
 
-    await getUSDPrice()
+    // await getUSDPrice()
 }
 
 init()

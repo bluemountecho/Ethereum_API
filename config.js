48129681e09be084fb4346e043b4a5028d91aa8f
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-04 00:35:11 +0900

    Update config.js

--- config.js
+++ config.js
@@ -290,6 +290,8 @@ module.exports.fantom = {
     'PAIRV1': 68000,
     'PAIRV2': 1000,
     'PROXYCOUNT': 68,
+    'USD_ADDRESS': "0x04068da6c83afcfa0e13ba15a6696662335d5b75",
+    'ETH_ADDRESS': "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83",
 }
 
 module.exports.harmony = {

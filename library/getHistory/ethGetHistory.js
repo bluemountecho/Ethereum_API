4949b250d1c0d52081fee4ba09440a27fd0b60fb
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-03 22:43:04 +0900

    Update ethGetHistory.js

--- library/getHistory/ethGetHistory.js
+++ library/getHistory/ethGetHistory.js
@@ -1767,7 +1767,7 @@ async function createTables() {
         await knex.raw('\
             ALTER TABLE `' + tokensTableName + '`\
                 MODIFY tokenName text,\
-                MODIFY createdAt timestamp AFTER links,\
+                MODIFY createdAt timestamp NULL DEFAULT NULL AFTER links,\
                 RENAME COLUMN otherInfos TO coingeckoInfos,\
                 ADD COLUMN lastPrice double NOT NULL DEFAULT 0;\
         ')

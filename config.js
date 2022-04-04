0f528c638ad06c78a5a9eb4e786d0a8e7891df0e
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-04 14:03:38 +0000

    Update config.js

--- config.js
+++ config.js
@@ -215,6 +215,7 @@ module.exports.polygon = {
     'PROXYCOUNT': 40,
     'USD_ADDRESS': "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
     'ETH_ADDRESS': "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+    'lastBlockNumber': 26518437,
 }
 
 module.exports.arbitrum = {

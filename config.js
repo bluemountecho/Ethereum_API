5cfc51fa8f10f8355be72d68c0f86892abca8902
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-04 03:13:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -476,7 +476,7 @@ module.exports.celo = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0x918146359264c492bd6934071c6bd31c854edbc3",
     'ETH_ADDRESS': "0x471ece3750da237f93b8e339c536989b8978a438",
-    'lastBlockNumber': 12197460,
+    'lastBlockNumber': 12203940,
 }
 
 module.exports.xdai = {

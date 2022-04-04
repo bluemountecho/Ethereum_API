68ab67af17eaa9ec93286d909b5cfac74937de71
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-04 13:30:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -529,6 +529,7 @@ module.exports.hsc = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0xd16babe52980554520f6da505df4d1b124c815a7",
     'ETH_ADDRESS': "0x3eff9d389d13d6352bfb498bcf616ef9b1beac87",
+    'lastBlockNumber': 9613627,
 }
 
 module.exports.boba = {

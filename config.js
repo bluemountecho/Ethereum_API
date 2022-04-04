3f4ee9ee4ab127bca749b03bbe96ff2049655f5a
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-04 11:16:04 +0900

    test

--- config.js
+++ config.js
@@ -239,6 +239,7 @@ module.exports.arbitrum = {
     'PROXYCOUNT': 10,
     'USD_ADDRESS': "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
     'ETH_ADDRESS': "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+    'lastBlockNumber': 8021809,
 }
 
 module.exports.optimism = {

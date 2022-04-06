f7c32a09287dfdbff8670d46f49fedc471f79876
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-06 23:21:51 +0900

    Update ethereum_api.sql

--- ethereum_api.sql
+++ ethereum_api.sql
@@ -11,11 +11,28 @@
  Target Server Version : 100417
  File Encoding         : 65001
 
- Date: 30/01/2022 13:11:04
+ Date: 06/04/2022 23:21:44
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bsc_daily
+-- ----------------------------
+DROP TABLE IF EXISTS `bsc_daily`;
+CREATE TABLE `bsc_daily`  (
+  `PAIRADDRESS` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `SWAPAT` timestamp(0) NULL DEFAULT NULL,
+  `AVGPRICE` double NULL DEFAULT NULL,
+  `MAXPRICE` double NULL DEFAULT NULL,
+  `MINPRICE` double NULL DEFAULT NULL,
+  `VOLUME0` double NULL DEFAULT NULL,
+  `VOLUME1` double NULL DEFAULT NULL,
+  `TOTALVOLUME0` double NULL DEFAULT NULL,
+  `TOTALVOLUME1` double NULL DEFAULT NULL,
+  `SWAPCOUNT` int(11) NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for bsc_live
@@ -66,141 +83,6 @@ CREATE TABLE `bsc_past`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Table structure for bsc_past1
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past1`;
-CREATE TABLE `bsc_past1`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past2
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past2`;
-CREATE TABLE `bsc_past2`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past3
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past3`;
-CREATE TABLE `bsc_past3`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past4
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past4`;
-CREATE TABLE `bsc_past4`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past5
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past5`;
-CREATE TABLE `bsc_past5`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past6
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past6`;
-CREATE TABLE `bsc_past6`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past7
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past7`;
-CREATE TABLE `bsc_past7`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past8
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past8`;
-CREATE TABLE `bsc_past8`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for bsc_past9
--- ----------------------------
-DROP TABLE IF EXISTS `bsc_past9`;
-CREATE TABLE `bsc_past9`  (
-  `pairAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapPrice` double NULL DEFAULT NULL,
-  `swapAmount0` double NULL DEFAULT NULL,
-  `swapAmount1` double NULL DEFAULT NULL,
-  `swapMaker` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapTransactionHash` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `swapAt` timestamp(0) NULL DEFAULT NULL,
-  `isBuy` tinyint(4) NULL DEFAULT 0
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
 -- Table structure for bsc_tokens
 -- ----------------------------
 DROP TABLE IF EXISTS `bsc_tokens`;
@@ -209,9 +91,56 @@ CREATE TABLE `bsc_tokens`  (
   `tokenSymbol` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `tokenName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `tokenDecimals` int(255) NULL DEFAULT NULL,
+  `sourceCode` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `otherInfos` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `createdAt` timestamp(0) NULL DEFAULT NULL,
+  `totalSupply` double NULL DEFAULT NULL,
+  `totalHolders` int(11) NULL DEFAULT NULL,
+  `holders` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `links` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   PRIMARY KEY (`tokenAddress`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for eth_changes
+-- ----------------------------
+DROP TABLE IF EXISTS `eth_changes`;
+CREATE TABLE `eth_changes`  (
+  `tokenAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price24h` double NULL DEFAULT 0,
+  `price12h` double NULL DEFAULT 0,
+  `price6h` double NULL DEFAULT 0,
+  `price2h` double NULL DEFAULT 0,
+  `price1h` double NULL DEFAULT 0,
+  `price30m` double NULL DEFAULT 0,
+  `price5m` double NULL DEFAULT 0,
+  `trans24h` int(11) NULL DEFAULT 0,
+  `trans12h` int(11) NULL DEFAULT 0,
+  `trans6h` int(11) NULL DEFAULT 0,
+  `trans2h` int(11) NULL DEFAULT 0,
+  `trans1h` int(11) NULL DEFAULT 0,
+  `trans30m` int(11) NULL DEFAULT 0,
+  `trans5m` int(11) NULL DEFAULT 0,
+  PRIMARY KEY (`tokenAddress`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for eth_daily
+-- ----------------------------
+DROP TABLE IF EXISTS `eth_daily`;
+CREATE TABLE `eth_daily`  (
+  `PAIRADDRESS` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `SWAPAT` timestamp(0) NULL DEFAULT NULL,
+  `AVGPRICE` double NULL DEFAULT NULL,
+  `MAXPRICE` double NULL DEFAULT NULL,
+  `MINPRICE` double NULL DEFAULT NULL,
+  `VOLUME0` double NULL DEFAULT NULL,
+  `VOLUME1` double NULL DEFAULT NULL,
+  `TOTALVOLUME0` double NULL DEFAULT NULL,
+  `TOTALVOLUME1` double NULL DEFAULT NULL,
+  `SWAPCOUNT` int(11) NULL DEFAULT NULL,
+  INDEX `PAIRADDRESS`(`PAIRADDRESS`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for eth_live
@@ -262,6 +191,20 @@ CREATE TABLE `eth_past`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Table structure for eth_token_daily
+-- ----------------------------
+DROP TABLE IF EXISTS `eth_token_daily`;
+CREATE TABLE `eth_token_daily`  (
+  `TOKENADDRESS` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `SWAPAT` timestamp(0) NULL DEFAULT NULL,
+  `AVGPRICE` double NULL DEFAULT NULL,
+  `MAXPRICE` double NULL DEFAULT NULL,
+  `MINPRICE` double NULL DEFAULT NULL,
+  `VOLUME` double NULL DEFAULT NULL,
+  `SWAPCOUNT` int(11) NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for eth_tokens
 -- ----------------------------
 DROP TABLE IF EXISTS `eth_tokens`;
@@ -270,7 +213,13 @@ CREATE TABLE `eth_tokens`  (
   `tokenSymbol` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `tokenName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `tokenDecimals` int(255) NULL DEFAULT NULL,
+  `sourceCode` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `otherInfos` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `createdAt` timestamp(0) NULL DEFAULT NULL,
+  `totalSupply` double NULL DEFAULT NULL,
+  `totalHolders` int(11) NULL DEFAULT NULL,
+  `holders` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `links` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   PRIMARY KEY (`tokenAddress`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 

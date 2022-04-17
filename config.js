113011e85b6feae6196ed7f044af8caf9e846a6c
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-18 02:16:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -409,7 +409,9 @@ module.exports.moonriver = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d",
     'ETH_ADDRESS': "0x98878b06940ae243284ca214f92bb71a2b032b8a",
-    'lastBlockNumber': 1607774,
+    'lastBlockNumber': 1716522,
+    'ETH_DECIMAL': 18,
+    'ETH_ID': 12,
 }
 
 module.exports.metis = {

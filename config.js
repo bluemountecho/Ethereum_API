f339a4ea292114d82059cf2a6c97d2f68431631d
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-28 02:02:05 +0900

    Update config.js

--- config.js
+++ config.js
@@ -216,7 +216,7 @@ module.exports.bsc = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0xe9e7cea3dedca5984780bafc599bd69add087d56",
     'ETH_ADDRESS': "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
-    'lastBlockNumber': 17028810,
+    'lastBlockNumber': 17316083,
     'ETH_DECIMAL': 18,
     'ETH_ID': 2,
 }

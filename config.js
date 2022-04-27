c50f683fa0b1b9309b2f9a9802579939f8ebe51d
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-28 02:39:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -662,7 +662,7 @@ module.exports.smartbch = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0x7b2b3c5308ab5b2a1d9a94d20d35ccdf61e05b72",
     'ETH_ADDRESS': "0x3743ec0673453e5009310c727ba4eaf7b3a1cc04",
-    'lastBlockNumber': 4072859,
+    'lastBlockNumber': 4221407,
     'ETH_DECIMAL': 18,
     'ETH_ID': 20,
 }

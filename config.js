31672aebf3b0e4189f3cb9d5806352915c15262e
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-28 02:07:04 +0900

    Update config.js

--- config.js
+++ config.js
@@ -833,7 +833,7 @@ module.exports.elastos = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0xa06be0f5950781ce28d965e5efc6996e88a8c141",
     'ETH_ADDRESS': "0x517e9e5d46c1ea8ab6f78677d6114ef47f71f6c4",
-    'lastBlockNumber': 12055915,
+    'lastBlockNumber': 12222093,
     'ETH_DECIMAL': 18,
     'ETH_ID': 29,
 }

1855d67714fe79497a41787b14bc4b5e2becfac2
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-28 02:21:21 +0900

    Update config.js

--- config.js
+++ config.js
@@ -491,7 +491,7 @@ module.exports.moonbeam = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0x818ec0a7fe18ff94269904fced6ae3dae6d6dc0b",
     'ETH_ADDRESS': "0xacc15dc74880c9944775448304b263d191c6077f",
-    'lastBlockNumber': 839994,
+    'lastBlockNumber': 909680,
     'ETH_DECIMAL': 18,
     'ETH_ID': 11,
 }

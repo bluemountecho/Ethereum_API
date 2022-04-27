bf090fd1363627a3a193f77040fb8a86d21a202a
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-28 02:24:40 +0900

    Update config.js

--- config.js
+++ config.js
@@ -567,7 +567,7 @@ module.exports.okc = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0x382bb369d343125bfb2117af9c149795c6c65c50",
     'ETH_ADDRESS': "0x8f8526dbfd6e38e3d8307702ca8469bae6c56c15",
-    'lastBlockNumber': 10387155,
+    'lastBlockNumber': 10620644,
     'ETH_DECIMAL': 18,
     'ETH_ID': 15,
 }

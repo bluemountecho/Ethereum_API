df90339b5223ca0a662c01c163f2f13261b7b45c
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-04-30 13:03:21 +0900

    test

--- config.js
+++ config.js
@@ -216,7 +216,7 @@ module.exports.bsc = {
     'PROXYCOUNT': 68,
     'USD_ADDRESS': "0xe9e7cea3dedca5984780bafc599bd69add087d56",
     'ETH_ADDRESS': "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
-    'lastBlockNumber': 17370838,
+    'lastBlockNumber': 17371188,
     'ETH_DECIMAL': 18,
     'ETH_ID': 2,
 }

02147e93790da97934fdd9276fab4dad21538ef3
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-06-02 15:27:09 +0800

    Update updateInfos.js

--- library/getLiveInfos/updateInfos.js
+++ library/getLiveInfos/updateInfos.js
@@ -343,8 +343,8 @@ async function getCoinGeckoInfo() {
 }
 
 async function init() {
-    getCoinsList()
-    // getCoinGeckoInfo()
+    // getCoinsList()
+    getCoinGeckoInfo()
     // getTotalSupply()
 }
 

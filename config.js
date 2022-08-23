a526caebb86b3093c215bcbd9393577e9bef275b
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-08-23 19:59:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports.PROXY = [
-    "185.200.179.221:8800",
     "158.69.205.15:3128",
     "158.69.134.188:3128",
     "167.114.195.106:3128",
@@ -214,7 +213,7 @@ module.exports.bsc = {
     'TRANSACTIONV2': 200,
     'PAIRV1': 68000,
     'PAIRV2': 1000,
-    'PROXYCOUNT': 1,
+    'PROXYCOUNT': 68,
     'USD_ADDRESS': "0xe9e7cea3dedca5984780bafc599bd69add087d56",
     'ETH_ADDRESS': "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
     'lastBlockNumber': 17371188,

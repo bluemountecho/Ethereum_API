9663edc35e19b20b8ff0279a74e1445a44105a3f
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-09-07 11:03:32 +0800

    Update ethGetLiveInfos.js

--- library/getLiveInfos/ethGetLiveInfos.js
+++ library/getLiveInfos/ethGetLiveInfos.js
@@ -687,6 +687,13 @@ async function init() {
         var coinsFuncs = []
         var results = []
         var id = 0
+        const topics = [
+            '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9',
+            '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822',
+            '0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118',
+            '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67',
+            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        ]
 
         // var results = await Promise.all([
             // web3s[1].eth.getPastLogs({
@@ -723,6 +730,27 @@ async function init() {
         //         topics: ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef']
         //     }),
         // ])
+
+        for (var i = 0; i < 5; i ++) {
+            while (true) {
+                try {
+                    results[i] = await web3s[id].eth.getPastLogs({
+                        fromBlock: lastBlockNumber,
+                        toBlock: blockNumber,
+                        topics: [topics[i]]
+                    })
+                    console.log(results)
+                    id ++
+    
+                    break
+                } catch (err) {
+                    console.log('ERROR(MAIN): ' + id)
+                    id ++
+                }
+    
+                if (id >= proxyCnt) id = 0
+            }
+        }
 
         while (true) {
             try {

178f8bbca7b72454c026b3774f95535f329ada6f
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-09-07 11:01:13 +0800

    Update ethGetLiveInfos.js

--- library/getLiveInfos/ethGetLiveInfos.js
+++ library/getLiveInfos/ethGetLiveInfos.js
@@ -738,7 +738,8 @@ async function init() {
                             '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
                         ]
                     ]
-                })[0]
+                })
+                console.log(results)
                 id ++
 
                 break

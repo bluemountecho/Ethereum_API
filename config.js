d443cabf43586581c6f7821e350349a7b9fe2eeb
talentMan726 <talentboy726@gmail.com>
2022-10-02 09:24:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -459,7 +459,7 @@ module.exports.harmony = {
 
 module.exports.cronos = {
     'web3Providers': [
-        'https://grpc.cronos.org/',
+        'https://node.croswap.com/rpc',
     ],
     'endPointType': 1,
     'FROMBLOCK': 0,

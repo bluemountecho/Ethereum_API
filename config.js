39591b1673b9378562e432cfb891ff39858c3075
talentMan726 <talentboy726@gmail.com>
2022-10-02 10:24:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -459,7 +459,7 @@ module.exports.harmony = {
 
 module.exports.cronos = {
     'web3Providers': [
-        'https://evm.cronos.org',
+        'https://rpc.vvs.finance',
     ],
     'endPointType': 1,
     'FROMBLOCK': 0,

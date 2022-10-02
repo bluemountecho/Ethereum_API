6220d756fc6ff0902649455237cb75288486fc47
talentMan726 <talentboy726@gmail.com>
2022-10-02 09:14:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -399,7 +399,7 @@ module.exports.optimism = {
 
 module.exports.avalanche = {
     'web3Providers': [
-        'curl https://avalanche-mainnet-rpc.allthatnode.com/ext/bc/C/rpc',
+        'https://avalanche-mainnet-rpc.allthatnode.com/ext/bc/C/rpc',
     ],
     'endPointType': 1,
     'FROMBLOCK': 0,

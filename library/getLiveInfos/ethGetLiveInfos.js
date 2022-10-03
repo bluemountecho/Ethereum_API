bef4b5e9cb71f798dd2bd8988cd91d046db6e097
talentMan726 <talentboy726@gmail.com>
2022-10-03 22:18:31 +0800

    Update ethGetLiveInfos.js

--- library/getLiveInfos/ethGetLiveInfos.js
+++ library/getLiveInfos/ethGetLiveInfos.js
@@ -707,11 +707,11 @@ async function init() {
         var results = []
         var id = 0
         const topics = [
-            '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9',
+            // '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9',
             // '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822',
             // '0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118',
             // '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67',
-            // '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
         ]
 
         // var results = await Promise.all([

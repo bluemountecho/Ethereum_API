f02f13762d5b5a2a01aead90a92e627b3f89981a
AnouKhamtan0726 <talentboy726@gmail.com>
2022-11-25 17:01:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -475,7 +475,7 @@ module.exports.cronos = {
     'ETH_DECIMAL': 18,
     'ETH_ID': 9,
     'ExplorerSite': 'https://cronoscan.com',
-    'BLOCK_RANGE': 1,
+    'BLOCK_RANGE': 0,
 }
 
 module.exports.aurora = {
